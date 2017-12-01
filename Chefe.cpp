c7dec3e0a3a8a9f755ba6ca3a4149f8087ab2938
gurocha7 <33911429+gurocha7@users.noreply.github.com>
2017-12-01 00:21:57 -0200

    Create Chefe.cpp

---
+++ Chefe.cpp
@@ -0,0 +1,49 @@
+//
+// Created by gusta on 30/11/2017.
+//
+
+#include "Chefe.h"
+#include <vector>
+void Chefe::exibirMenu(){
+    int op;
+    cout << "----------------------------------------" <<endl;
+    cout << "1 - CADASTRAR FUNCIONARIO"<<endl;
+    cout << "2 - FUNCIONARIOS CADASTRADOS"<<endl;
+    cout << "3 - CHECAR PONTO"<<endl;
+    cout << "4 - CALCULO DO SALARIO(HORAS TRABALHADAS)"<<endl;
+    cout << "5 - RETORNAR A TELA PRINCIPAL"<<endl;
+    cout << "-----------------------------------------" <<endl;
+    cout << "INFORME SUA OPCAO: "; cin >> op;
+    this->acao = op;
+}
+void Chefe::controle() {
+    if(acao==5) return ;
+    else if(acao==1){//cadastrar funcionario
+        cadastrarFuncionario();
+    }
+    else if(acao==2){//imprime os funcionarios cadastrados
+        funcionariosCadastrados();
+    }
+}
+void Chefe::cadastrarFuncionario() {
+    string nome,funcao,tipo,usuario,senha;
+    double salarioPorHora; int horasTrabalhadas,qtd;
+    cout << "Informe a quantidade de funcionarios que deseja cadastrar: "; cin>>qtd;
+    <vector>Funcionario<qtd>;
+    for(int i=0;i<qtd;i++){
+        cout << "Informe seu nome: "; cin >>nome;
+        cout << "Informe sua funcao: "; cin >> funcao;
+        cout << "Informe seu tipo: "; cin >> tipo;
+        cout << "Informe seu usuario: "; cin >> usuario;
+        cout << "Informe sua senha: "; cin >> senha;
+        cout << "Informe seu salario por hora: "; cin >>salarioPorHora;
+        cout << "Informe a quantidade de horas trabalhadas: "; cin >> horasTrabalhadas;
+        Funcionario[i](nome,funcao,tipo,usuario,senha,salarioPorHora,horasTrabalhadas);
+    }
+}
+void Chefe::funcionariosCadastrados() {
+    int x ;
+}
+double Chefe::calcularSalario() {
+
+}
